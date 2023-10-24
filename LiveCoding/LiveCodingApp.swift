19ede83ee6938d7e5c804bcf97f180bb88958376
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-10-24 11:25:35 -0300

    Initial Commit

---
+++ LiveCoding/LiveCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCodingApp.swift
+//  LiveCoding
+//
+//  Created by Alliston Aleixo on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
